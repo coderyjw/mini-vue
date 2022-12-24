9d8929c86f3540f6e7e0994dab2f5601f81427be
叶继伟 <yejw6@asiainfo.com>
2022-12-25 03:39:17 +0800

    引入prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 结尾无分号
+  semi: false,
+  // 全部使用单引号
+  singleQuote: true,
+  // 每行长度为 80
+  printWidth: 80,
+  // 不添加尾随 ， 号
+  trailingComma: 'none',
+  // 省略箭头函数括号
+  arrowParens: 'avoid'
+};
