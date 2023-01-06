688368b85abcb8e9ba10c0ff735631790163052b
叶继伟 <yejw6@asiainfo.com>
2023-01-06 23:49:11 +0800

    区分处理 ELEMENT 节点的各种属性挂载

--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -1,6 +1,7 @@
 import { isOn } from '@vue/shared'
 import { patchClass } from './modules/class'
-
+import { patchDOMProp } from './modules/props'
+import { patchAttr } from './modules/attrs'
 /**
  * 为 prop 进行打补丁操作
  */
@@ -11,7 +12,33 @@ export const patchProp = (el, key, prevValue, nextValue) => {
     // TODO: style
   } else if (isOn(key)) {
     // TODO: 事件
+  } else if (shouldSetAsProp(el, key)) {
+    // 通过 DOM Properties 指定
+    patchDOMProp(el, key, nextValue)
   } else {
-    // TODO: 其他属性
+    // 其他属性
+    patchAttr(el, key, nextValue)
   }
+}
+
+/**
+ * 判断指定元素的指定属性是否可以通过 DOM Properties 指定
+ */
+function shouldSetAsProp(el: Element, key: string) {
+  // #1787, #2840 表单元素的表单属性是只读的，必须设置为属性 attribute
+  if (key === 'form') {
+    return false
+  }
+
+  // #1526 <input list> 必须设置为属性 attribute
+  if (key === 'list' && el.tagName === 'INPUT') {
+    return false
+  }
+
+  // #2766 <textarea type> 必须设置为属性 attribute
+  if (key === 'type' && el.tagName === 'TEXTAREA') {
+    return false
+  }
+
+  return key in el
 }
--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -958,6 +958,28 @@ var Vue = (function (exports) {
     }
 
     /**
+     * 通过 DOM Properties 指定属性
+     */
+    function patchDOMProp(el, key, value) {
+        try {
+            el[key] = value;
+        }
+        catch (e) { }
+    }
+
+    /**
+     * 通过 setAttribute 设置属性
+     */
+    function patchAttr(el, key, value) {
+        if (value == null) {
+            el.removeAttribute(key);
+        }
+        else {
+            el.setAttribute(key, value);
+        }
+    }
+
+    /**
      * 为 prop 进行打补丁操作
      */
     var patchProp = function (el, key, prevValue, nextValue) {
@@ -966,8 +988,33 @@ var Vue = (function (exports) {
         }
         else if (key === 'style') ;
         else if (isOn(key)) ;
-        else ;
+        else if (shouldSetAsProp(el, key)) {
+            // 通过 DOM Properties 指定
+            patchDOMProp(el, key, nextValue);
+        }
+        else {
+            // 其他属性
+            patchAttr(el, key, nextValue);
+        }
     };
+    /**
+     * 判断指定元素的指定属性是否可以通过 DOM Properties 指定
+     */
+    function shouldSetAsProp(el, key) {
+        // #1787, #2840 表单元素的表单属性是只读的，必须设置为属性 attribute
+        if (key === 'form') {
+            return false;
+        }
+        // #1526 <input list> 必须设置为属性 attribute
+        if (key === 'list' && el.tagName === 'INPUT') {
+            return false;
+        }
+        // #2766 <textarea type> 必须设置为属性 attribute
+        if (key === 'type' && el.tagName === 'TEXTAREA') {
+            return false;
+        }
+        return key in el;
+    }
 
     var rendererOptions = extend({ patchProp: patchProp }, nodeOps);
     var renderer;
